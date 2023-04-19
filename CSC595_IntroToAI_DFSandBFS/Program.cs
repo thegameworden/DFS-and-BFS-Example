3b93e4b842354f31c692c4d1af005a2f5b7c7846
Ethan Worden <ethanworden@live.com>
2023-04-19 10:56:25 -0700

    first commit

---
+++ CSC595_IntroToAI_DFSandBFS/Program.cs
@@ -0,0 +1,108 @@
+﻿using CSC595_IntroToAI_DFSandBFS;
+using System.Xml.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Max value each node can have: ");
+        int maxVal = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Number of levels the tree should have: ");
+        int levels = Convert.ToInt32(Console.ReadLine());
+        Console.Write("What value should the algorithms try to find: ");
+        int val = Convert.ToInt32(Console.ReadLine());
+        Tree tree =new(maxVal, levels);
+
+        tree.printTree();
+
+        Console.WriteLine("\nStarting PreOrder:");
+        Console.WriteLine(dfsPreOrder(tree.Root, val));
+        Console.WriteLine("-----------------");
+
+        Console.WriteLine("\nStarting PostOrder:");
+        Console.WriteLine(dfsPostOrder(tree.Root, val));
+        Console.WriteLine("-----------------");
+
+        Console.WriteLine("\nStarting InOrder:");
+        Console.WriteLine(dfsInOrder(tree.Root, val));
+        Console.WriteLine("-----------------");
+
+        Console.WriteLine("\nStarting BFS:");
+        Console.WriteLine(bfs(tree.Root, val));
+        Console.WriteLine("-----------------");
+
+    }
+
+    public static bool dfsPreOrder(Node node, int val) {
+        if (node == null) return false;
+
+        Console.Write($"{node.Val} ");
+        if (node.Val == val) return true; 
+
+        return dfsPreOrder(node.Left, val) || dfsPreOrder(node.Right, val);
+        
+
+    }
+
+
+    public static bool dfsPostOrder(Node node, int val)
+    {
+        if (node == null) return false;
+
+        if (dfsPostOrder(node.Left, val)) return true;
+
+        if (dfsPostOrder(node.Right, val)) return true;
+
+        Console.Write($"{node.Val} ");
+        if (node.Val == val) return true;
+        
+        return false;
+    }
+
+    public static bool dfsInOrder(Node node, int val)
+    {
+        if (node == null) return false;
+
+
+        if (dfsInOrder(node.Left, val)) return true;
+
+
+        Console.Write($"{node.Val} ");
+        if (node.Val == val) return true;
+
+        if (dfsInOrder(node.Right, val)) return true;
+
+        return false;
+    }
+    public static bool bfs(Node node, int val)
+    {
+        if (node == null) return false;
+
+        Queue<Node> q = new();
+
+        q.Enqueue(node);
+        while (q.Count != 0)
+        {
+            Node n = q.Dequeue();
+            Console.Write($"{n.Val} ");
+
+            if (n.Val == val) return true;
+
+            if (n.Left != null) q.Enqueue(n.Left);
+            if (n.Right != null) q.Enqueue(n.Right);
+        }
+        return false;
+
+    }
+
+    public static void print(string algo, List<Node> visited)
+    {
+        Console.WriteLine($"Using the algorithm '{algo}', this is the order of the nodes that were visited");
+        Console.Write("[ ");
+        foreach(Node n in visited)
+        {
+            Console.Write($"{n.Val} ");
+        }
+        Console.WriteLine(" ]");
+    }
+}
---
+++ CSC595_IntroToAI_DFSandBFS/Tree.cs
@@ -0,0 +1,77 @@
+﻿
+
+namespace CSC595_IntroToAI_DFSandBFS
+{
+
+    public class Node
+    {
+        int val;
+        Node left;
+        Node right;
+
+        public Node(int val)
+        {
+            this.val = val;
+        }
+
+        public int Val { get => val; set => val = value; }
+        public Node Left { get => left; set => left = value; }
+        public Node Right { get => right; set => right = value; }
+    }
+    public class Tree
+    {
+        Node root;
+        int maxVal;
+        int levels;
+        public Tree(int maxVal, int levels)
+        {
+            this.maxVal = maxVal;
+            this.levels = levels;
+            Random random= new Random();
+            root = new(random.Next(maxVal + 1));
+            populate(root,1);
+            
+
+        
+        }
+
+        private void populate(Node node,int currLevel) {
+            if (currLevel == levels) return;
+
+            Random random = new Random();
+            node.Left = new Node(random.Next(maxVal + 1));
+            node.Right = new Node(random.Next(maxVal + 1));
+
+            populate(node.Left,currLevel+1);
+            populate(node.Right,currLevel+1);
+
+        }
+
+        public void printTree()
+        {
+            Console.WriteLine("Printing Tree");
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while(queue.Count > 0)
+            {
+               Node n = queue.Dequeue();
+                if (n.Left == null && n.Right == null) continue;
+
+                Console.Write($"Node {n.Val} -> [ ");
+                if (n.Left != null) {
+                    Console.Write($"{n.Left.Val} ");
+                    queue.Enqueue(n.Left); 
+                }
+                if (n.Right != null)
+                {
+                    Console.Write($"{n.Right.Val} ");
+                    queue.Enqueue(n.Right);
+                }
+                Console.WriteLine("]");
+            }
+
+        }
+
+        internal Node Root { get => root; set => root = value; }
+    }
+}
